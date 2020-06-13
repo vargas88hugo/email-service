6c28ca18c75e6e4ba18cbc81302099b6a4aa51df
Hugo Vargas <vargas88hugo@gmail.com>
2020-06-12 20:28:46 -0500

    [Test] Deployment 5

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Test1: Please go to the path /api to use Swagger Documentation';
+    return 'Test2: Please go to the path /api to use Swagger Documentation';
   }
 }
