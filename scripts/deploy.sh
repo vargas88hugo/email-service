0fe5882e16db5d52759d9d4e31d8ef925a0ad33f
Hugo Vargas <vargas88hugo@gmail.com>
2020-06-12 20:19:11 -0500

    [Update] Add Store Secret to sensitive information

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 cd /var/www/email-service/
+export DB_USER=$(aws ssm get-parameter --name db_user --with-decryption --output text --query Parameter.Value)
+export DB_PASSWORD=$(aws ssm get-parameter --name db_password --with-decryption --output text --query Parameter.Value)
+export SECRET=$(aws ssm get-parameter --name secret --with-decryption --output text --query Parameter.Value)
+export SPARKPOST_KEY=$(aws ssm get-parameter --name sparkpost_key --with-decryption --output text --query Parameter.Value)
+export SENDGRID_KEY=$(aws ssm get-parameter --name sendgrid_key --with-decryption --output text --query Parameter.Value)
 docker-compose build
 docker-compose up -d
-docker-compose exec db psql -U postgres -d postgres -c "ALTER USER postgres WITH PASSWORD 'password';" || :
+docker-compose exec db psql -U postgres -d postgres -c "ALTER USER $DB_USER WITH PASSWORD '$DB_PASSWORD';" || :
 docker-compose exec db psql -U postgres -c "CREATE DATABASE emailservice;" || :
 docker-compose exec db psql -U postgres -c "CREATE DATABASE test;" || :
--- scripts/script.sh
+++ scripts/script.sh
@@ -2,6 +2,6 @@
 
 docker-compose build
 docker-compose up -d
-docker-compose exec db psql -U postgres -d postgres -c "ALTER USER postgres WITH PASSWORD 'password';"
+docker-compose exec db psql -U postgres -d postgres -c "ALTER USER postgres WITH PASSWORD '$password';"
 docker-compose exec db psql -U postgres -c "CREATE DATABASE emailservice;"
 docker-compose exec db psql -U postgres -c "CREATE DATABASE test;"
