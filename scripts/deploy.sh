e329f5cf3e070beae489c1bf9071216d5ad5acdc
Hugo Vargas <vargas88hugo@gmail.com>
2020-06-12 20:38:24 -0500

    [Test] Deployment 6

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd /var/www/email-service/
+cd /var/www/email-service
 export DB_USER=$(aws ssm get-parameter --name db_user --with-decryption --output text --query Parameter.Value)
 export DB_PASSWORD=$(aws ssm get-parameter --name db_password --with-decryption --output text --query Parameter.Value)
 export SECRET=$(aws ssm get-parameter --name secret --with-decryption --output text --query Parameter.Value)
@@ -11,3 +11,4 @@ docker-compose up -d
 docker-compose exec db psql -U postgres -d postgres -c "ALTER USER $DB_USER WITH PASSWORD '$DB_PASSWORD';" || :
 docker-compose exec db psql -U postgres -c "CREATE DATABASE emailservice;" || :
 docker-compose exec db psql -U postgres -c "CREATE DATABASE test;" || :
+docker-compose up -d
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Test2: Please go to the path /api to use Swagger Documentation';
+    return 'Test3: Please go to the path /api to use Swagger Documentation';
   }
 }
